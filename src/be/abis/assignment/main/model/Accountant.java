b7a197386579594e9544acdfae8eae327a2039bd
Swaleha Mulla <swalehamulla7834@gmail.com>
2025-06-10 12:25:58 +0200

    first commit with Person, Student,Accountant and Order

--- src/be/abis/assignment/main/model/Accountant.java
+++ src/be/abis/assignment/main/model/Accountant.java
@@ -1,4 +1,16 @@
 package be.abis.assignment.main.model;
 
-public class Accountant {
+public class Accountant extends Person {
+    public Accountant(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+
+    public void orderSandwich(String sandwichName, Enum TypeOfBread, boolean needVegetable){
+        Sandwich s1 = new Sandwich();
+        Order o1 = new Order(s1,this);
+    }
+
+    public double calculatePrice(){
+        return 0.0;
+    }
 }
