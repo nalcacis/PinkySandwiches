cd191d494b244bec81bc4a3f4637afd429320364
Swaleha Mulla <swalehamulla7834@gmail.com>
2025-06-10 12:25:58 +0200

    first commit with Person, Student,Accountant and Order

--- src/be/abis/assignment/main/model/Order.java
+++ src/be/abis/assignment/main/model/Order.java
@@ -1,4 +1,24 @@
 package be.abis.assignment.main.model;
 
 public class Order {
+    Person person;
+    Sandwich sandwich;
+
+    public Order(Sandwich sandwich, Person person) {
+        this.sandwich = sandwich;
+        this.person = person;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+    public Sandwich getSandwich() {
+        return sandwich;
+    }
+    public void setSandwich(Sandwich sandwich) {
+        this.sandwich = sandwich;
+    }
 }
