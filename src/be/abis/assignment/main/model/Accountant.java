2ba031fdd21a6177baa5687ad0d4e48ab6d9d888
Swaleha Mulla <swalehamulla7834@gmail.com>
2025-06-10 13:32:45 +0200

    newer updates

--- src/be/abis/assignment/main/model/Accountant.java
+++ src/be/abis/assignment/main/model/Accountant.java
@@ -1,12 +1,14 @@
 package be.abis.assignment.main.model;
 
+import be.abis.assignment.main.enumeration.TypeOfBread;
+
 public class Accountant extends Person {
     public Accountant(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
-    public void orderSandwich(String sandwichName, Enum TypeOfBread, boolean needVegetable){
-        Sandwich s1 = new Sandwich();
+    public void orderSandwich(String sandwichName, TypeOfBread typeOfBread, boolean vegetable ){
+        Sandwich s1 = new Sandwich(typeOfBread,  vegetable, sandwichName, "1000");
         Order o1 = new Order(s1,this);
     }
 
